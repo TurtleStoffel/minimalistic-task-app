92fb88db117ca76f2a007773e457245846eb859e
TurtleStoffel <me@turtlestoffel.com>
2025-06-18 20:34:19 +0200

    Move task from Task Inbox to Task Queue

--- main.js
+++ main.js
@@ -75,6 +75,33 @@ ipcMain.on('remove-task-inbox', (event, idx) => {
   }
   BrowserWindow.getAllWindows().forEach(win => win.webContents.send('tasks-updated'));
 });
+ipcMain.on('move-task-to-queue', (event, idx) => {
+  const tasksPath = path.join('C:', 'Users', 'stefa', 'coding', 'task-queue', 'task-queue.json');
+  const inboxPath = path.join('C:', 'Users', 'stefa', 'coding', 'task-queue', 'Task Inbox.json');
+  let inbox = [];
+  let tasks = [];
+  try {
+    if (fs.existsSync(inboxPath)) {
+      inbox = JSON.parse(fs.readFileSync(inboxPath, 'utf-8'));
+    }
+  } catch (e) {
+    inbox = [];
+  }
+  try {
+    if (fs.existsSync(tasksPath)) {
+      tasks = JSON.parse(fs.readFileSync(tasksPath, 'utf-8'));
+    }
+  } catch (e) {
+    tasks = [];
+  }
+  if (idx >= 0 && idx < inbox.length) {
+    const [task] = inbox.splice(idx, 1);
+    tasks.push(task);
+    fs.writeFileSync(inboxPath, JSON.stringify(inbox, null, 2), 'utf-8');
+    fs.writeFileSync(tasksPath, JSON.stringify(tasks, null, 2), 'utf-8');
+  }
+  BrowserWindow.getAllWindows().forEach(win => win.webContents.send('tasks-updated'));
+});
 
 app.whenReady().then(createWindow);
 
